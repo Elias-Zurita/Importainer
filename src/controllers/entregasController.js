4e7d3c3fa27397084043ea2b6809f9b5ea36be5a
Elias-Zurita <eliass.zurita@gmail.com>
2023-06-07 13:02:06 -0300

    controller detalle entrega ok

--- src/controllers/entregasController.js
+++ src/controllers/entregasController.js
@@ -3,7 +3,7 @@ const path = require("path");
 const db = require ("../db/models"); // nos permite utilizar la base de datos
 const {Op} = require("sequelize");
 
-const proyectosController = {
+const entregasController = {
     list: function(req, res) {
         db.Entrega.findAll({
             include: [{association: "Modelo"}]  
@@ -11,7 +11,18 @@ const proyectosController = {
             .then(function(entregas){   
                 res.render("entregas/entregasList", {entregas:entregas})
             })
+    },
+    detalle: function(req, res) {
+        let pedidoEntrega = db.Entrega.findByPk(req.params.id, {
+            include:[{association: "Modelo"}]});
+            
+        let pedidoModelo = db.Modelo.findAll();
+        
+        Promise.all([pedidoEntrega, pedidoModelo])
+            .then(function(values){
+                res.render("entregas/entregaDetalle", {entrega:values[0], modelos: values[1]})
+            })
     }
  }
 
-module.exports = proyectosController;
+module.exports = entregasController;
--- src/routes/entregasRoutes.js
+++ src/routes/entregasRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const entregasController = require("../controllers/entregasController");
 
 router.get("/list", entregasController.list);
+router.get("/detalle/:id", entregasController.detalle);
 
 
 module.exports = router;
