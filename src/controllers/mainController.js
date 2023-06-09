a25d03093898cef5d4090ce4c76c71ecd24d146f
Elias-Zurita <eliass.zurita@gmail.com>
2023-06-09 16:55:29 -0300

    estilos home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,15 @@
 const path = require("path");
+const db = require ("../db/models"); // nos permite utilizar la base de datos
+const {Op} = require("sequelize");
 
 const controller = {
     index: (req, res) =>{
-        res.render('index')    // renderiza el index  
+        db.Entrega.findAll({
+            include: [{association: "Modelo"}]  
+        })
+            .then(function(entregas){   
+                res.render('index', {entregas:entregas})
+            })
     },
     sistemaConstructivo: (req, res) =>{
         res.render('sistemaConstructivo')    
