b5564cd6ef41483431400fdc7cca2e12b00abcaa
Elias-Zurita <eliass.zurita@gmail.com>
2023-06-12 16:39:35 -0300

    router proyectos

--- src/controllers/proyectosController.js
+++ src/controllers/proyectosController.js
@@ -4,14 +4,134 @@ const db = require ("../db/models"); // nos permite utilizar la base de datos
 const {Op} = require("sequelize");
 
 const proyectosController = {
-    list: function(req, res) {
+    categorias: function(req, res) {
         db.Proyecto.findAll({
             include: [{association: "Categoria"}]
         })
             .then(function(proyectos){
-                res.render("proyectos/proyectosList", {proyectos:proyectos})
+                res.render("proyectos/proyectosCategorias", {proyectos:proyectos})
             })
-    }
+    },
+    classic: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '1'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    comfort: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '2'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    premium: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '3'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    casacampo: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '4'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    casatiny: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '5'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    duplex: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '6'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    combo: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '7'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    exteriores: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '8'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    emprendimientos: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '9'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
+    oficinas: function (req, res, next) {
+        let proyectoToFind = req.query.search;
+        db.Proyecto.findAll({
+            include: [{association: "Categoria"}],
+            where: {
+                categoria_id: '10'
+            }
+        })
+        .then(function (proyectos) {
+            res.render("proyectos/proyectosList", {proyectos:proyectos})
+        })
+    },
  }
 
 module.exports = proyectosController;
--- src/routes/proyectosRoutes.js
+++ src/routes/proyectosRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 const proyectosController = require("../controllers/proyectosController");
 
-router.get("/list", proyectosController.list);
+router.get("/categorias", proyectosController.categorias);
+router.get("/classic", proyectosController.classic);
+router.get("/comfort", proyectosController.comfort);
+router.get("/premium", proyectosController.premium);
+router.get("/casacampo", proyectosController.casacampo);
+router.get("/casatiny", proyectosController.casatiny);
+router.get("/duplex", proyectosController.duplex);
+router.get("/combo", proyectosController.combo);
+router.get("/exteriores", proyectosController.exteriores);
+router.get("/emprendimientos", proyectosController.oficinas);
+router.get("/oficinas", proyectosController.classic);
 
 
 module.exports = router;
